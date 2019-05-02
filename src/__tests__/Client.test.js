2911785f06e75a8d2841fdc73f5ae293e60fc07f
angela <angela@aviak.ca>
2019-05-01 20:52:24 -0600

    fixed test for card add to use a real representation

--- src/__tests__/Client.test.js
+++ src/__tests__/Client.test.js
@@ -16,9 +16,14 @@ test('Test Client creates a deck successfully', () => {
 test('Test Client creates a card successfully', () => {
   const testCardId = '1809fd82-686a-4205-b0b5-51db95c0fd29';
   const testDeckId = '1809fd82-686a-4205-b0b5-51db95c0fd22';
-  fetch.once(JSON.stringify({ id: '1809fd82-686a-4205-b0b5-51db95c0fd29' }));
-  Client.createCard(testDeckId, 'bird', 'el pájaro').then((id) => {
-    expect(id).toEqual(testCardId);
+  fetch.once(JSON.stringify({
+    EN_word: 'test',
+    ES_word: 'prueba',
+    card_id: testCardId,
+    deck_id: testDeckId,
+  }));
+  Client.createCard(testDeckId, 'bird', 'el pájaro').then((cardId) => {
+    expect(cardId).toEqual(testCardId);
   });
   expect(fetch.mock.calls.length).toEqual(2);
   expect(fetch.mock.calls[1][0]).toEqual(`api/deck/${testDeckId}/card?EN_word=bird&ES_word=el pájaro`);
