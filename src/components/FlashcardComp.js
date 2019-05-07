2267febe769516aece8dccee1acd3a5bc99bc672
angela <angela@aviak.ca>
2019-05-06 18:37:15 -0600

    better naming, a few css changes

--- src/components/FlashcardComp.js
+++ src/components/FlashcardComp.js
@@ -2,11 +2,12 @@ import React from 'react';
 import NavBarComp from './NavBarComp';
 import ActionBarComp from './ActionBarComp';
 
-class DashboardComp extends React.Component {
+class FlashcardComp extends React.Component {
   constructor() {
     super();
     this.state = {
       deckList: [],
+      
     };
   }
 
@@ -14,12 +15,12 @@ class DashboardComp extends React.Component {
     return (
       <div>
         <ActionBarComp />
-        <div>
-          workspace here
+        <div className="WorkArea">
+          <h2>workspace here</h2>
         </div>
         <NavBarComp />
       </div>
     );
   }
 }
-export default DashboardComp;
+export default FlashcardComp;
